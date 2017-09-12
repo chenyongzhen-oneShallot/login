5b4df647b8d9a6c6cad9974aaf6680fb6f9699c0
cyzAdmin <891354032@qq.com>
2017-09-12 11:42:33 +0800

    注册登录

---
+++ src/com/mingrisoft/util/DBConfig.java
@@ -0,0 +1,8 @@
+package com.mingrisoft.util;
+
+public interface DBConfig {
+
+    String databaseName = "db_database01";// 数据库名称
+    String username = "root";// 数据库用户名
+    String password = "user@123";// 数据库密码
+}
